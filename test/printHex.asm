f223a7f1d6b09eb5dab690ca1608cd670620aad0
向晚今天几点睡 <113411296+Alex4210987@users.noreply.github.com>
2023-10-19 19:05:11 +0800

    modify and add

--- test/printHex.asm
+++ test/printHex.asm
@@ -1,16 +1,14 @@
 .model small
+.stack
 .data
-    bin_number dw 0101110110011110b ; 二进制数A，以十六进制表示
-    hex_buffer db 4 DUP ('$') ; 存储十六进制结果的缓冲区
-    hex_output db "Hex: $"
+    bin_number dw 1111111111111111b
 
 .code
-    org 100h
 start:
-    ; 方法1: 使用寄存器传递参数
-    mov ax, [bin_number]  ; 将二进制数加载到寄存器AX
+    mov ax, @data
+    mov ds, ax
+    mov ax, bin_number
     call binToHex
-    call printHex
 
     ; 退出程序
     mov ah, 4Ch  ; 设置DOS功能号，表示退出程序
@@ -20,35 +18,25 @@ binToHex proc
     ; 输入：AX 中存储二进制数
     ; 输出：hex_buffer 中存储十六进制结果
     mov cx, 4
-    lea di, hex_buffer
 
 convertLoop:
-    shr ax, 4      ; 右移4位，处理下一个四位二进制数
-    pushf          ; 保存标志寄存器状态
-    and al, 0Fh    ; 保留低四位
-    mov ah, 0      ; 清空高四位
-    cmp al, 9
-    jbe convertHexDigit  ; 如果小于或等于9，转化为字符
-    add al, 7      ; 否则，转化为A-F的字符
+    mov dl, ah     ; 将结果存储到DL
+    and dl, 0F0h    ; 将高四位二进制数置0
+    shr dl, 4
+    cmp dl, 09h    ; 判断是否小于或等于9
+    jbe printHexDigit  ; 如果小于或等于9，转化为字符
+    add dl, 7      ; 否则，转化为A-F的字符
 
-convertHexDigit:
-    add al, '0'     ; 将数字字符转化为ASCII码
-    stosb           ; 存储到缓冲区
-    popf            ; 恢复标志寄存器状态
+printHexDigit:
+    add dl, '0'    ; 转化为ASCII码
+    push ax        ; 保存AX
+    mov ah, 2      ; 设置DOS功能号，表示显示字符
+    int 21h        ; 调用21h中断，显示字符
+    pop ax         ; 恢复AX
+    shl ax, 4      ; 右移4位，处理下一个四位二进制数
     loop convertLoop
+
     ret
 binToHex endp
-
-printHex proc
-    ; 输入：hex_buffer 中的十六进制数据
-    ; 输出：在屏幕上显示十六进制数据
-    mov ah, 09h     ; 设置DOS功能号，表示显示字符串
-    lea dx, hex_output
-    int 21h
-
-    lea dx, hex_buffer
-    int 21h
-    ret
-printHex endp
 
 end start
